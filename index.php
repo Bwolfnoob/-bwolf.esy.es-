d71f3798a03e2fb635fcc4e12ee9ef2c4a717ab6
Bwolf <Bwolfnoob@gmail.com>
2015-11-08 14:49:33 -0200

    a
    a

--- index.php
+++ index.php
@@ -2,13 +2,29 @@
 <html>
 <head>
 <meta charset="UTF-8">
-<title>Title of the document</title>
+<title>Bwolf PHP WebPage</title>
+   <link rel="stylesheet" href="css/main.css">
 </head>
 
 <body>
-<header>  
-    Welcome to PHP Bwolf Page
+    <header>  
+        <div>
+            PHP Bwolf Page
+        </div>
+        <nav>
+            <a href="" >Home</a>
+             <a href="" >SecondPage</a>
+            <a href="" >ThirdPage</a>
+        </nav>
     </header>
+    <article>
+        <?php>
+        for ($i = 0 ; $i < 10;$i++)
+        {
+            echo "Var:$i";
+        }    
+        </php>
+    </article>
 </body>
 
 </html> 
